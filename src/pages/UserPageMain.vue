5b5dff848dc241a115de4a18afe9dd38e36bdeae
deni <deni@debian>
2022-11-04 11:53:09 -0500

    generating id for table rows

--- src/pages/UserPageMain.vue
+++ src/pages/UserPageMain.vue
@@ -27,10 +27,10 @@
     <div class="col-9 q-pr-md q-pt-md q-pl-md row content-center">
       <q-table
         class="col"
-        title="Imported table"
+        title="Квартиры"
         :rows="rows"
         :columns="columns"
-        row-key="0"
+        :row-key="createId"
         :selected-rows-label="getSelectedString"
         selection="multiple"
         v-model:selected="selected"
@@ -51,17 +51,80 @@ export default {
       rows: [],
       columns: [
         {
-          name: "desc",
+          name: "location",
           align: "center",
-          label: "Index",
+          label: "Местоположение",
           field: "0",
           sortable: true,
         },
         {
-          name: "value",
+          name: "rooms",
           align: "center",
-          label: "Value",
+          label: "Количество комнат",
           field: "1",
+          sortable: true,
+        },
+        {
+          name: "category",
+          align: "center",
+          label: "Сегмент",
+          field: "2",
+          sortable: true,
+        },
+        {
+          name: "floors",
+          align: "center",
+          label: "Этажность дома",
+          field: "3",
+          sortable: true,
+        },
+        {
+          name: "walls_material",
+          align: "center",
+          label: "Материал стен",
+          field: "4",
+          sortable: true,
+        },
+        {
+          name: "floor",
+          align: "center",
+          label: "Этаж расположения",
+          field: "5",
+          sortable: true,
+        },
+        {
+          name: "total_area",
+          align: "center",
+          label: "Площадь квартиры, кв.м",
+          field: "6",
+          sortable: true,
+        },
+        {
+          name: "kitchen_area",
+          align: "center",
+          label: "Площадь кухни, кв.м",
+          field: "7",
+          sortable: true,
+        },
+        {
+          name: "is_balcony",
+          align: "center",
+          label: "Наличие балкона/лоджии",
+          field: "8",
+          sortable: true,
+        },
+        {
+          name: "metro_distance",
+          align: "center",
+          label: "Удаленность от станции метро, мин. пешком",
+          field: "9",
+          sortable: true,
+        },
+        {
+          name: "condition",
+          align: "center",
+          label: "Состояние",
+          field: "10",
           sortable: true,
         },
       ],
@@ -73,12 +136,18 @@ export default {
       // console.log(event);
       let xlsxfile = event ? event[0] : null;
       readXlsxFile(xlsxfile).then((rows) => {
+        rows.shift();
+        console.log(rows);
         this.rows = rows;
         for (let i = 0; i < this.rows.length; ++i) {
           // this.rows[i] = Object.assign(this.rows[i]);
           console.log(this.rows[i]);
         }
       });
+    },
+
+    createId(row) {
+      return row.reduce((partialSum, a) => partialSum + a, 0);
     },
 
     getSelectedString() {
@@ -96,6 +165,7 @@ export default {
         type: "negative",
         message: "Количество комнат должно быть уникально",
       });
+      // this.$router.push("")
     },
 
     // onFileChange(event) {
